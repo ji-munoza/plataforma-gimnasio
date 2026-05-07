1f458ab7f5a8ca06a2adc2af601168f669cba666
ji-munoza <ji.munoza@duocuc.cl>
2026-05-06 20:38:11 -0400

    repositorio

---
+++ ms_socio/src/main/java/cl/plataforma_gimnasio/ms_socio/MsSocioApplication.java
@@ -0,0 +1,13 @@
+package cl.plataforma_gimnasio.ms_socio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSocioApplication.class, args);
+	}
+
+}
