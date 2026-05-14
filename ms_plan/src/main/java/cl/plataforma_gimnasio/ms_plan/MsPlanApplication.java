4c97ea193c0b72492bacea95163eb9e0221a9b88
ji.munoza@duocuc.cl <ji.munoza@duocuc.cl>
2026-05-14 08:13:06 -0400

    creado ms plan

---
+++ ms_plan/src/main/java/cl/plataforma_gimnasio/ms_plan/MsPlanApplication.java
@@ -0,0 +1,13 @@
+package cl.plataforma_gimnasio.ms_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPlanApplication.class, args);
+	}
+
+}
---
+++ ms_plan/src/test/java/cl/plataforma_gimnasio/ms_plan/MsPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.plataforma_gimnasio.ms_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
